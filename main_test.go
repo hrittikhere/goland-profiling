85245e2cd0918b8e617bd1244ca345c534cc9896
Hrittik Roy <67012359+hrittikhere@users.noreply.github.com>
2022-11-18 12:40:46 +0530

    Update Functinos

--- main_test.go
+++ main_test.go
@@ -13,18 +13,16 @@ func TestBlock(t *testing.T) {
 
 func TestFibRecursive(t *testing.T) {
 
-	for i := 0; i < 100; i++ { // running it a 1000 times
-		if FibRecursive(31) != 1346269 {
-			//The above lines test the FibRecursive function to see if it returns the correct result when given the input 31. If it does not return 1346269, it will throw an error.
-			t.Error("Incorrect!")
-		}
+	if FibRecursive(40) != 102334155 {
+		//The above lines test the FibRecursive function to see if it returns the correct result when given the input 31. If it does not return 1346269, it will throw an error.
+		t.Error("Incorrect!")
+
 	}
 }
 
 func TestFib(t *testing.T) {
-	for i := 0; i < 1000000; i++ { // running it a 1000 times
-		if Fib(30) != 832040 {
-			t.Error("Incorrect!")
-		}
+	if Fib(30) != 832040 {
+		t.Error("Incorrect!")
+
 	}
 }
